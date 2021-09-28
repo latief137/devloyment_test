1cba842689bcc71b9903fe5b4966fc3d59e3ea2e
latief137 <latief137@gmail.com>
2021-09-28 15:45:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { DatePicker, Button, version } from 'antd';
+import './App.css'
+import "antd/dist/antd.css";
+import  { MyContext } from './context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Stage1 from "./components/stage_1";
+import Stage2 from "./components/stage_2";
+
+class App extends React.Component {
+
+  static contextType = MyContext;
+
+  render() {
+    return (
+    <div className="wrapper">
+      <div className="center-wrapper">
+        <h1>Who pays the bill 2</h1>
+        { this.context.state.stage === 1 ?
+           <Stage1/>
+           :
+           <Stage2/>
+        }
+      </div>
+      <div>
+      <div className="App">
+    <h1>antd version: {version}</h1>
+    <DatePicker />
+    <Button type="primary" style={{ marginLeft: 8 }}>
+      Primary Button
+    </Button>
+  </div>
+
+      </div>
     </div>
-  );
+
+    )
+  }
 }
 
 export default App;
